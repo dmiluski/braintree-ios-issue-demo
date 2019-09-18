ce9e88872f7d96847579f95442df91a27b5806d0
Dane Miluski <dane.miluski@venmo.com>
2019-09-18 13:31:24 -0700

    Initial Project Commit

---
+++ BraintreeIntegrationDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BraintreeIntegrationDemo
+//
+//  Created by Miluski, Dane on 9/18/19.
+//  Copyright © 2019 Venmo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
